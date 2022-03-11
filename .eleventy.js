cfc143617346415f3892609e36bc0a9dfb186398
Robert C. Kalajian Jr <rkalajian@gmail.com>
2022-03-11 13:28:22 -0500

    Favicon

--- .eleventy.js
+++ .eleventy.js
@@ -130,7 +130,7 @@ module.exports = function (eleventyConfig) {
     return moment(date).toISOString();
   });
   eleventyConfig.addFilter('dateReadable', date => {
-    return moment(date).utc()..tz('America/New_York').format('LL')
+    return moment(date).utc().tz('America/New_York').format('LL')
   });
   eleventyConfig.addFilter('dateTimeReadable', date => {
     return moment(date).utc().tz('America/New_York').format(' MMMM Do YYYY, h:mm:ss a')
