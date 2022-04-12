2fd2168c099b3a8490bcb84a40631006b73db5e9
Robert C. Kalajian Jr <rkalajian@gmail.com>
2022-04-12 11:03:52 -0400

    Removal of more stuff

--- src/data/mastodon.js
+++
@@ -1,8 +0,0 @@
-const Parser = require('rss-parser');
-const parser = new Parser();
-
-let rss_feed = 'https://mastodon.social/users/devartty.rss';
-
-module.exports = async function() {
-	return await parser.parseURL(rss_feed);
-};
